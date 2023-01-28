4d34b92847a9fb90567b14c997cef2b9e998c583
Somendra Sharma <somendra23@gmail.com>
2023-01-28 18:49:38 -0500

    service project going in

---
+++ src/main/java/com/samj/prj/service/App.java
@@ -0,0 +1,13 @@
+package com.samj.prj.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
