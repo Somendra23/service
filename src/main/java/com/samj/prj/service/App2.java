bf346d57478ca801ba34c77addd096369024ed12
Somendra Sharma <somendra23@gmail.com>
2023-01-29 09:36:14 -0500

    Add App2

---
+++ src/main/java/com/samj/prj/service/App2.java
@@ -0,0 +1,13 @@
+package com.samj.prj.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2 
+{
+    public static void main( String[] args )
+    {
+       System.out.println("adding in branchX a new test case!!");
+    }
+}
